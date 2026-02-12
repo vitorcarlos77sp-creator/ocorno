0681673af2008aa3869e4f95892b3908da17f782
vitorcarlos <vitorcarlos77sp@gmail.com>
2026-02-12 19:12:44 +0000

    deixa ai bicho

---
+++ src/importaaipraver.jsx
@@ -0,0 +1,12 @@
+
+function qualaurlcorno(){
+	return "https://baby-api.corno2.online";
+}
+
+
+function qualaurlcorno2(){
+    return process.env.REACT_APP_FAST_API;
+}
+
+
+export default qualaurlcorno;
